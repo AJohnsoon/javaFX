9d5ce54595ef847350f18a12a405bf610f688068
ajohnsoon <allanjohnson.leal@gmail.com>
2022-10-07 21:19:10 -0300

    refactor: change return panel to show message

--- src/java/main/application/gui/SampleController.java
+++ src/java/main/application/gui/SampleController.java
@@ -2,7 +2,7 @@ package src.java.main.application.gui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import src.java.main.application.gui.util.Alerts;
 
 import java.util.Random;
@@ -10,7 +10,8 @@ import java.util.Random;
 public class SampleController {
 
     @FXML
-    private Button clickHere;
+    private Label myMessage;
+
 
     @FXML
     public void onClickHereAction() {
@@ -21,17 +22,14 @@ public class SampleController {
         int value = rd1 - rd2;
 
         if(value > 0){
-            Alerts.showAlert("Congratulation", "Gzz", "congratulations the number has been accepted", Alert.AlertType.INFORMATION);
-            System.out.println(value);
+            Alerts.showAlert("Congratulation", "Positive Number", "Congratulations the number has been accepted", Alert.AlertType.INFORMATION);
         }
         else if (value < 0){
-            Alerts.showAlert("Error", "Ops!", "Ops ! Number is not included in the criteria", Alert.AlertType.ERROR);
-            System.out.println(value);
+            Alerts.showAlert("Error", "Ops! Negative Number", "Ops ! Number is not included in the criteria", Alert.AlertType.ERROR);
         }
         else {
             Alerts.showAlert("UUUh", "Almost", "Number was in the middle\n", Alert.AlertType.WARNING);
-            System.out.println(value);
         }
-
+        myMessage.setText(String.valueOf(value));
     }
 }
